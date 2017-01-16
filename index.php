d20cc4d855463a5000717ff59ec8852c376f83fa
jarnielcataluna <jarnielcataluna@searchoptmedia.com>
2017-01-16 20:07:04 +0800

    Calculator Test Form

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<form method="POST" action="exec/calculate-investment-goal.php">
+    <input type="text" name="targetGoal" placeholder="targetGoal"><br>
+    <input type="text" name="monthlyIncome" placeholder="monthlyIncome"><br>
+    <input type="text" name="investmentDuration" placeholder="investmentDuration"><br>
+    <button type="submit">Calculate</button>
+</form>
+</body>
+</html>
